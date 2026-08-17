6664460def496fd203f3ad378de8c2384ca71628
Bouke <bouke@iridis.nl>
2026-08-17 21:08:06 +0200

    Added missing test case.

--- Phonebook_MCP.Tests/PhonebookSearchServiceTests.cs
+++ Phonebook_MCP.Tests/PhonebookSearchServiceTests.cs
@@ -14,6 +14,7 @@ public sealed class PhonebookSearchServiceTests
     [InlineData("")]
     [InlineData(" ")]
     [InlineData("S")]
+    [InlineData(" S ")]
     [InlineData("%a")]
     [InlineData("a_")]
     public void IsValidSearchName_RejectsInvalidSearchTerms(string? name)
